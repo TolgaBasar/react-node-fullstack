080d42588f2781719e44bee7e5d05a62d5981a12
TolgaBasar <tolgabasar406@gmail.com>
2025-07-06 22:38:53 +0300

    Frontend production build and vercel config

--- src/utils/axiosConfig.js
+++ src/utils/axiosConfig.js
@@ -1,15 +1,7 @@
 import axios from "axios";
 
-const api = axios.create({
-  baseURL: process.env.REACT_APP_API || "http://localhost:5000/api",
+const instance = axios.create({
+  baseURL: process.env.REACT_APP_API_URL,
 });
 
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-  if (token) {
-    config.headers["Authorization"] = `Bearer ${token}`;
-  }
-  return config;
-});
-
-export default api;
+export default instance;
